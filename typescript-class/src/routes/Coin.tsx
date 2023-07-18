a2f3eb019577b4d0aee1881e9a0137363815d0f0
권소정 <flfk33@naver.com>
2023-07-18 22:51:43 +0900

    23/07/18 TIL

--- typescript-class/src/routes/Coin.tsx
+++ typescript-class/src/routes/Coin.tsx
@@ -1,8 +1,17 @@
-import { Route, Switch, useLocation, useParams } from "react-router-dom"
+import {
+  Route,
+  Switch,
+  useLocation,
+  useParams,
+  Link,
+  useRouteMatch,
+} from "react-router-dom"
 import { styled } from "styled-components"
 import { useEffect, useState } from "react"
 import Chart from "./Chart"
 import Price from "./Price"
+import { useQuery } from "react-query"
+import { fetchCoinInfo, fetchCoinTickers } from "../api"
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -46,6 +55,28 @@ const OverviewItem = styled.div`
 `
 const Description = styled.p`
   margin: 20px 0px;
+`
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
 `
 
 interface RouteParams {
@@ -113,27 +144,40 @@ interface PriceData {
 }
 
 function Coin() {
-  const [loading, setLoading] = useState(true)
   const { coinId } = useParams<RouteParams>()
   const { state } = useLocation<RouteState>()
-  const [info, setInfo] = useState<InfoData>()
-  const [priceInfo, setPriceInfo] = useState<PriceData>()
-  useEffect(() => {
-    ;(async () => {
-      const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json() // 캡슐화
-      const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json()
-      setInfo(infoData)
-      setPriceInfo(priceData)
-      setLoading(false)
-    })()
-  }, [coinId])
+  const priceMatch = useRouteMatch("/:coinId/price")
+  const chartMatch = useRouteMatch("/:coinId/chart")
+  const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(
+    ["info", coinId],
+    () => fetchCoinTickers(coinId)
+  )
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
+    ["tickers", coinId],
+    () => fetchCoinInfo(coinId)
+  )
+  // const [loading, setLoading] = useState(true)
+  // const [info, setInfo] = useState<InfoData>()
+  // const [priceInfo, setPriceInfo] = useState<PriceData>()
+
+  // useEffect(() => {
+  //   ;(async () => {
+  //     const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json() // 캡슐화
+  //     const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json()
+  //     setInfo(infoData)
+  //     setPriceInfo(priceData)
+  //     setLoading(false)
+  //   })()
+  // }, [coinId])
+  const loading = infoLoading || tickersLoading
   return (
     <Container>
       <Header>
-        <Title> {state?.name || "Loading..."} </Title>
-        <Title>{state?.name ? state.name : loading ? "Loading..." : info?.name}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {/* {loading ? <Loader>Loading...</Loader> : null} */}
       {loading ? (
         <Loader>Loading...</Loader>
       ) : (
@@ -141,33 +185,42 @@ function Coin() {
           <Overview>
             <OverviewItem>
               <span>Rank:</span>
-              <span>{info?.rank}</span>
+              <span>{infoData?.rank}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Symbol:</span>
-              <span>${info?.symbol}</span>
+              <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Open Source:</span>
-              <span>{info?.open_source ? "Yes" : "No"}</span>
+              <span>{infoData?.open_source ? "Yes" : "No"}</span>
             </OverviewItem>
           </Overview>
-          <Description>{info?.description}</Description>
+          <Description>{infoData?.description}</Description>
           <Overview>
             <OverviewItem>
               <span>Total Suply:</span>
-              <span>{priceInfo?.total_supply}</span>
+              <span>{tickersData?.total_supply}</span>
             </OverviewItem>
             <OverviewItem>
               <span>Max Supply:</span>
-              <span>{priceInfo?.max_supply}</span>
+              <span>{tickersData?.max_supply}</span>
             </OverviewItem>
           </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
           <Switch>
-            <Route path={`/${coinId}/price`}>
+            <Route path={`/:coinId/price`}>
               <Price />
             </Route>
-            <Route path={`/${coinId}/chart`}>
+            <Route path={`/:coinId/chart`}>
               <Chart />
             </Route>
           </Switch>
