7549e3eb5ac124f8e353b70a310ee8b1e57718fb
권소정 <flfk33@naver.com>
2023-07-24 08:35:21 +0900

    23/07/24 TIL

--- typescript-class/src/routes/Coin.tsx
+++ typescript-class/src/routes/Coin.tsx
@@ -144,7 +144,11 @@ interface PriceData {
   }
 }
 
-function Coin() {
+interface ICoinProps {
+  isDark: boolean
+}
+
+function Coin({ isDark }: ICoinProps) {
   const { coinId } = useParams<RouteParams>()
   const { state } = useLocation<RouteState>()
   const priceMatch = useRouteMatch("/:coinId/price")
@@ -202,7 +206,7 @@ function Coin() {
             </OverviewItem>
             <OverviewItem>
               <span>Price:</span>
-              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
+              <span>${tickersData?.quotes?.USD?.price?.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
@@ -230,7 +234,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart coinId={coinId} />
+              <Chart isDark={isDark} coinId={coinId} />
             </Route>
           </Switch>
         </>
